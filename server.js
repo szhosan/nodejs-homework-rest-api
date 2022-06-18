b9ba86c87bd953eca2a46218bc6e6f80d123d679
@s_zhsn <s.zhosan@gmail.com>
2022-06-18 22:26:14 +0300

    hw-03-process

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const app = require("./app");
+const MongoClient = require("mongodb").MongoClient;
+const { MONGO_URL, PORT = 3001 } = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const start = async () => {
+  const client = await MongoClient.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = client.db();
+  const Contacts = db.collection("contacts");
+  const contacts = await Contacts.find({}).toArray();
+  console.log(contacts);
+  app.listen(PORT, () => {
+    console.log("Server running. Use our API on port: " + PORT);
+  });
+};
+
+start();
